86a64d5169e5f795435a553a28724772342a2a63
PC12-IFLAB <PC12-IFLAB>
2023-03-15 16:21:22 +0700

    menambahkan class code buah

--- modul4_1302213034/Program.cs
+++ modul4_1302213034/Program.cs
@@ -1,4 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("ujang");
+﻿using System;
+using System.Runtime.Serialization;
 
+namespace modul4_13002213034
+{
+    internal class program
+    {
+        public enum buah
+        {
+            apel,
+            aprikot,
+            alpukat,
+            pisang,
+            paprika,
+            blackberry,
+            ceri,
+            kelapa,
+            jagung,
+            kurma,
+            durian,
+            anggur,
+            melon,
+            semangka,         
+        };
+        public class getkodebuah
+        {
+            public static string getkode(buah buah)
+            {
+                string[] kodebuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+                return kodebuah[(int)buah];
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            buah buah = buah.kelapa;
+            string kode = getkodebuah.getkode(buah);
+            Console.WriteLine("buah :"+ buah + "\ndengan kode :"+kode);
+        }
+    }
+}
